ccadb7808ecba1395fd8f89d965599ee747d3ed8
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-02-16 19:33:05 +0100

    Update staircaselighting.ino
    Moved for to a function of its own. Functions, but does not have ability  to  decide what way the light turns on/off

--- staircaselighting/staircaselighting.ino
+++ staircaselighting/staircaselighting.ino
@@ -1,45 +1,72 @@
 
-int pirPinUp=2;
-int pirPinDown=3;
-bool pirStatUp=false;
-bool pirStatNed=false;  
+// Value of Pir pins
+int pirPinUp = 2;
+int pirPinDown = 3;
+
+// Variables for determening if the sensors are triggered
+bool pirStatOpp = false;
+bool pirStatNed = false;  
   
-int LEDPins[] = {12,13};
+//Values for the logic about light
+int LEDPins[] = {12,13}; //Change in here for light pins for steps
 int numberOfElements = sizeof(LEDPins) / sizeof(LEDPins[0]);
+
 void setup() {
-pinMode(12,OUTPUT);
-pinMode(13,OUTPUT);
+  //Output LED
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
 
-pinMode(pirPinUp,INPUT);
-pinMode(pirPinDown,INPUT);
+  //Input PIR
+  pinMode(pirPinUp,INPUT);
+  pinMode(pirPinDown,INPUT);
 
-Serial.begin(9600);
-Serial.println(numberOfElements);
+  //Serial
+  Serial.begin(9600);
+
+  //Confirmation of element amount
+  Serial.println(numberOfElements);
 }
 
 void loop() {
-  pirStatUp=digitalRead(pirPinUp);
-  delay(1000);
-  //pirStatDown=digitalRead(pirPinNDown);
-  Serial.print("Value before if: ");
-  Serial.println(pirStatUp);
+  //Reads sensors and sets variables
+  pirStatOpp=digitalRead(pirPinUp);
+  pirStatNed=digitalRead(pirPinDown);
 
-  if (pirStatUp == HIGH){
-  //Serial.println(numberOfElements);
-  delay(500);
-     for(int i = 0; i < numberOfElements ; i++){
-      digitalWrite(LEDPins[i] ,HIGH);
-      Serial.println("Active up");
-       delay(1000);
-    }
+  Serial.print("Value bool up before process: ");
+  Serial.print(pirStatOpp);
+  Serial.print(", and bool down: ");
+  Serial.println(pirStatNed);
 
+  //Statement to check if the sensor is triggered to turn on lights
+  if (pirStatOpp == HIGH){ 
+    lightloop(1,1);
+    //Delay between turn on and turn off
+    delay(10000);
+    lightloop(0,1);
+    Serial.println("For loop done");
+    delay(1000);
   }
+  //Not turn
   else{
-    digitalWrite(12,LOW);
-    digitalWrite(13,LOW);
     delay(1000);
   }
  
-Serial.println("Round over");
+Serial.println("Loop done");
+}
+
+void lightloop(int turn, int array[]) {
+  //Decides if the call is is for turn on or turn off
+  int val;
+  if (turn == 0){
+    val = LOW;
+  }
+  else {
+    val = HIGH;
+  }
+  //Loop for turning on/off light
+  for(int i = 0; i < numberOfElements ; i++){
+      digitalWrite(LEDPins[i] ,val);
+      delay(1000);
+    }
 }
 
