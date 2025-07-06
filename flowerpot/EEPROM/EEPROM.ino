4422a2833f2df824f8ab01fba7c062161ca55998
datamannen1013 <data0@rommesmo.no>
2025-07-06 17:20:07 +0200

    Altered EEPROM to work with password adn name of wifi

--- flowerpot/EEPROM/EEPROM.ino
+++ flowerpot/EEPROM/EEPROM.ino
@@ -5,6 +5,15 @@ const char BLYNK_AUTH[] = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 const char WIFI_SSID[]  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 const char WIFI_PASS[]  = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
+// Fixed sizes
+const int EEPROM_SIZE = 512;
+const int AUTH_ADDR = 0;
+const int AUTH_SIZE = 33;
+const int SSID_ADDR = AUTH_ADDR + AUTH_SIZE;
+const int SSID_SIZE = 33;
+const int PASS_ADDR = SSID_ADDR + SSID_SIZE;
+const int PASS_SIZE = 65;
+
 // Encryption key (change this to your own random value)
 const byte encryptionKey = 0xA7;  // Example key
 
@@ -18,80 +27,54 @@ byte decryptByte(byte data) {
 
 void setup() {
   Serial.begin(115200);
-  EEPROM.begin(512);
+  EEPROM.begin(EEPROM_SIZE);
   
   Serial.println("Starting EEPROM write with encryption...");
   
-  int addr = 0;
-  
-  // Write encrypted Blynk auth token
-  for(int i = 0; i < strlen(BLYNK_AUTH); i++) {
-    EEPROM.write(addr++, encryptByte(BLYNK_AUTH[i]));
-  }
-  EEPROM.write(addr++, encryptByte('\0'));
-  
-  while(addr < 33) {
-    EEPROM.write(addr++, encryptByte('\0'));
-  }
-  
-  // Write encrypted WiFi SSID
-  for(int i = 0; i < strlen(WIFI_SSID); i++) {
-    EEPROM.write(addr++, encryptByte(WIFI_SSID[i]));
-  }
-  EEPROM.write(addr++, encryptByte('\0'));
-  
-  while(addr < 65) {
-    EEPROM.write(addr++, encryptByte('\0'));
-  }
-  
-  // Write encrypted WiFi password
-  for(int i = 0; i < strlen(WIFI_PASS); i++) {
-    EEPROM.write(addr++, encryptByte(WIFI_PASS[i]));
-  }
-  EEPROM.write(addr++, encryptByte('\0'));
-  
-  while(addr < 129) {
-    EEPROM.write(addr++, encryptByte('\0'));
-  }
-  
-  if(EEPROM.commit()) {
+  writeEncrypted(AUTH_ADDR, BLYNK_AUTH, AUTH_SIZE);
+  writeEncrypted(SSID_ADDR, WIFI_SSID, SSID_SIZE);
+  writeEncrypted(PASS_ADDR, WIFI_PASS, PASS_SIZE);
+
+  if (EEPROM.commit()) {
     Serial.println("Encrypted credentials written successfully!");
   } else {
     Serial.println("ERROR! EEPROM commit failed");
   }
-  
+
   Serial.println("\nVerifying stored encrypted credentials:");
   verifyStoredCredentials();
 }
 
+void writeEncrypted(int startAddr, const char* input, int maxLen) {
+  int len = std::min<size_t>(strlen(input), static_cast<size_t>(maxLen - 1));
+  for (int i = 0; i < len; i++) {
+    EEPROM.write(startAddr + i, encryptByte(input[i]));
+  }
+  EEPROM.write(startAddr + len, encryptByte('\0'));
+  for (int i = len + 1; i < maxLen; i++) {
+    EEPROM.write(startAddr + i, encryptByte('\0'));
+  }
+}
+
+void readDecrypted(int startAddr, char* output, int maxLen) {
+  for (int i = 0; i < maxLen - 1; i++) {
+    output[i] = decryptByte(EEPROM.read(startAddr + i));
+  }
+  output[maxLen - 1] = '\0';  // Ensure null-termination
+}
+
 void verifyStoredCredentials() {
-  char storedAuth[33];
-  char storedSSID[33];
-  char storedPass[65];
-  
-  int addr = 0;
-  
-  // Read and decrypt auth token
-  for(int i = 0; i < 32; i++) {
-    storedAuth[i] = decryptByte(EEPROM.read(addr++));
-  }
-  storedAuth[32] = '\0';
-  
-  // Read and decrypt SSID
-  for(int i = 0; i < 32; i++) {
-    storedSSID[i] = decryptByte(EEPROM.read(addr++));
-  }
-  storedSSID[32] = '\0';
-  
-  // Read and decrypt password
-  for(int i = 0; i < 64; i++) {
-    storedPass[i] = decryptByte(EEPROM.read(addr++));
-  }
-  storedPass[64] = '\0';
-  
+  char storedAuth[AUTH_SIZE];
+  char storedSSID[SSID_SIZE];
+  char storedPass[PASS_SIZE];
+
+  readDecrypted(AUTH_ADDR, storedAuth, AUTH_SIZE);
+  readDecrypted(SSID_ADDR, storedSSID, SSID_SIZE);
+  readDecrypted(PASS_ADDR, storedPass, PASS_SIZE);
+
   Serial.println("Stored Auth Token: " + String(storedAuth));
   Serial.println("Stored SSID: " + String(storedSSID));
   Serial.println("Stored Password: " + String(storedPass));
 }
 
-void loop(){}
+void loop() {}
